5f7437617a33c45604ef7c1a6bab37073b546eb0
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2021-07-09 08:50:55 +0200

    6.2 test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int resultCalculator = calculator.add(5,8);
+        int resultCalculator2 = calculator.substract(20,15);
+
+        if (resultCalculator == 13) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        if (resultCalculator2 == 5) {
+            System.out.println("test 2 OK");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
+
+
