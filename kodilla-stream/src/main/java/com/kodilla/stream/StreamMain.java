b6f43e5b50fa6c0fa302ad9d438d0c8d54519a6c
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2021-07-15 09:28:42 +0200

    7.2 Zadanie: niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -26,5 +27,8 @@ public class StreamMain {
         poemBeautifier.beautify("This should be in \"ABC\" braces.", text -> "ABC " + text + " ABC");
         poemBeautifier.beautify("HARRY POTTER", text -> text.toLowerCase());
         poemBeautifier.beautify("So weird ", (text) -> text.repeat(2));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
